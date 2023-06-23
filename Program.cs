5c737ed549ceb23ac28796d4d7b3e3055376ccb7
Flameopathic <flameopathic@gmail.com>
2023-06-23 04:30:59 -0400

    Initial Commit

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using NAudio.CoreAudioApi;
+using Microsoft.Maui;
+
+MMDeviceEnumerator deviceEnumerator = new();
+MMDevice device = deviceEnumerator.GetDefaultAudioEndpoint(DataFlow.Render, Role.Multimedia);
+int leftVolume = (int)(device.AudioMeterInformation.PeakValues[0]);
+int rightVolume = (int)(device.AudioMeterInformation.PeakValues[1]);
+Console.WriteLine(leftVolume.ToString());
+Console.WriteLine(rightVolume.ToString());
